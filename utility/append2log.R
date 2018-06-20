2f0f2cdd8a7398d6360e89c73945f7691af4d990
nassuphis <nassuphis@users.noreply.github.com>
2018-06-20 14:53:47 +0100

    Create append2log.R

---
+++ utility/append2log.R
@@ -0,0 +1,10 @@
+require(stringi)
+
+append2log<-function(log_text)
+{
+  cat(
+    paste0(stri_trim(gsub("##|-","",capture.output(timestamp())))," : ",log_text,"\n"),
+    file="N:/Depts/Share/UK Alpha Team/Analytics/Rscripts/workflow.log",
+    append=TRUE
+  )
+}
