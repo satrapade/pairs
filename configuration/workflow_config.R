5ce103078afec7a8e40153751b560a1000f0f38b
nassuphis <nassuphis@users.noreply.github.com>
2018-06-20 19:54:53 +0100

    Update workflow_config.R

--- configuration/workflow_config.R
+++ configuration/workflow_config.R
@@ -8,6 +8,6 @@ database_source_id="1"
 database_start_date="'2015-05-28'"
 database_end_date="'2018-05-01'"
 database_root_bucket_name="'EqyBucket'"
-
+database_results_directory="N:/Depts/Share/UK Alpha Team/Analytics/db_cache/"
 
 
