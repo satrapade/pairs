7c6c00896e9fe31c1204af80ff5617d2ddc0de89
nassuphis <nassuphis@users.noreply.github.com>
2018-06-22 16:40:46 +0100

    Create fetch_risk_report.R

---
+++ utility/fetch_risk_report.R
@@ -0,0 +1,24 @@
+
+fetch_risk_report<-function(
+  report_name="what_happened_last_week",
+  target_directory="N:/Depts/Share/UK Alpha Team/Analytics/risk_reports"
+){
+  
+  report_url<-paste0(
+    "https://raw.githubusercontent.com/satrapade/pairs/master/risk_reports/",
+    report_name,
+    ".Rnw"
+  )
+  
+  report_fname<-paste0(
+    target_directory,
+    "/",
+    report_name,
+    ".Rnw"
+  )
+  
+  download.file(report_url,report_fname) 
+  
+}
+
+
