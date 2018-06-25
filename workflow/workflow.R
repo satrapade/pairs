a0f2b407915209079f29ce50333b4fdb0caf8665
nassuphis <nassuphis@users.noreply.github.com>
2018-06-25 12:23:48 +0100

    Update workflow.R

--- workflow/workflow.R
+++ workflow/workflow.R
@@ -2,14 +2,12 @@
 # daily workflow
 #
 require(stringi)
-append2log<-function(log_text,append=TRUE)
-{
-  cat(
-    paste0(stri_trim(gsub("##|-","",capture.output(timestamp())))," : ",log_text,"\n"),
-    file="N:/Depts/Share/UK Alpha Team/Analytics/Rscripts/workflow.log",
-    append=append
-  )
-}
+require(knitr)
+
+source("https://raw.githubusercontent.com/satrapade/utility/master/utility_functions.R")
+source("https://raw.githubusercontent.com/satrapade/pairs/master/utility/create_report.R")
+source("https://raw.githubusercontent.com/satrapade/pairs/master/utility/fetch_risk_report.R")
+source("https://raw.githubusercontent.com/satrapade/pairs/master/utility/append2log.R")
 
 append2log("workflow: start",append=FALSE)
 
@@ -62,9 +60,32 @@ system("Rscript \"N:/Depts/Share/UK Alpha Team/Analytics/Rscripts/create_pair_ic
 
 
 #
-append2log("workflow: create_risk_reports")
-system("Rscript \"N:/Depts/Share/UK Alpha Team/Analytics/Rscripts/create_risk_reports.R\"")
+# data has been produced, now make .PDF reports
+#
 
+#system("Rscript \"N:/Depts/Share/UK Alpha Team/Analytics/Rscripts/create_risk_reports.R\"")
+
+
+append2log("workflow: create_risk_reports")
+
+setwd("N:/Depts/Share/UK Alpha Team/Analytics/risk_reports")
+create_report("scrape_status")
+create_report("market_data_status")
+create_report("portfolio_summary")
+create_report("risk_plots")
+create_report("pair_risk_contribution")
+create_report("what_happened_last_week")
+create_report("sheet_scrape_report")
+create_report("custom_AC_report")
+create_report("custom_JR_report")
+create_report("custom_DH_report")
+create_report("custom_GJ_report")
+create_report("custom_ABC_report")
+create_report("custom_MC_report")
+create_report("fx_trend_report",push_to_directory ="N:/Depts/FI Currency/Quant/" )
+create_report("bank_pair_report")
+create_report("trailing_stop_report")
+create_report("duke_luke_drawdown")
 
 
 
