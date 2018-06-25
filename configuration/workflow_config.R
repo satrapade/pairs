ec5beef78137b92b9e8180ff1641fc1901399f09
nassuphis <nassuphis@users.noreply.github.com>
2018-06-25 15:00:46 +0100

    Update workflow_config.R

--- configuration/workflow_config.R
+++ configuration/workflow_config.R
@@ -3,6 +3,22 @@
 #
 #
 
+workflow = c(
+  "create_database_temp_tables",
+  "create_cix_uploads",
+  "create_portfolio_upload",
+  "create_market_data",
+  "create_portfolio_summary",
+  "create_market_data_intraday",
+  "intraday_fx",
+  "intraday_index_members",
+  "intraday_bank_pairs",
+  "create_tsne_grid",
+  "perform_sheet_scrape_to_db",
+  "create_pair_icons"
+)
+
+
 database_product_id="7"
 database_source_id="1"
 database_start_date="'2015-05-28'"
