f7a75cf412c13748910fb16835324298f19edb07
nassuphis <nassuphis@users.noreply.github.com>
2018-06-25 14:02:16 +0100

    Update workflow.R

--- workflow/workflow.R
+++ workflow/workflow.R
@@ -5,69 +5,30 @@ require(stringi)
 require(knitr)
 
 source("https://raw.githubusercontent.com/satrapade/utility/master/utility_functions.R")
+source("https://raw.githubusercontent.com/satrapade/pairs/master/utility/append2log.R")
 source("https://raw.githubusercontent.com/satrapade/pairs/master/utility/create_report.R")
 source("https://raw.githubusercontent.com/satrapade/pairs/master/utility/fetch_risk_report.R")
-source("https://raw.githubusercontent.com/satrapade/pairs/master/utility/append2log.R")
+source("https://raw.githubusercontent.com/satrapade/pairs/master/utility/source_workflow_step.R")
 
 append2log("workflow: start",append=FALSE)
 
 #
-append2log("workflow: create_database_temp_tables.R")
-system("Rscript \"N:/Depts/Share/UK Alpha Team/Analytics/Rscripts/create_database_temp_tables.R\"")
+source_workflow_step("create_database_temp_tables")
+source_workflow_step("create_cix_uploads")
+source_workflow_step("create_portfolio_upload")
+source_workflow_step("create_market_data")
+source_workflow_step("create_portfolio_summary")
+source_workflow_step("create_market_data_intraday")
+source_workflow_step("intraday_fx")
+source_workflow_step("intraday_index_members")
+source_workflow_step("intraday_bank_pairs")
+source_workflow_step("create_tsne_grid")
+source_workflow_step("perform_sheet_scrape_to_db")
+source_workflow_step("create_pair_icons")
+source_workflow_step("create_pair_icons")
 
 #
-append2log("workflow: create_cix_uploads")
-system("Rscript \"N:/Depts/Share/UK Alpha Team/Analytics/Rscripts/create_cix_uploads.R\"")
-
-#
-append2log("workflow: create_portfolio_upload")
-system("Rscript \"N:/Depts/Share/UK Alpha Team/Analytics/Rscripts/create_portfolio_upload.R\"")
-
-#
-append2log("workflow: create_market_data")
-system("Rscript \"N:/Depts/Share/UK Alpha Team/Analytics/Rscripts/create_market_data.R\"")
-
-
-#
-append2log("workflow: create_portfolio_summary")
-system("Rscript \"N:/Depts/Share/UK Alpha Team/Analytics/Rscripts/create_portfolio_summary.R\"")
-
-#
-append2log("workflow: create_market_data_intraday")
-system("Rscript \"N:/Depts/Share/UK Alpha Team/Analytics/Rscripts/create_market_data_intraday.R\"")
-
-#
-append2log("workflow: intraday_fx")
-system("Rscript \"N:/Depts/Share/UK Alpha Team/Analytics/Rscripts/intraday_fx.R\"")
-
-#
-append2log("workflow: intraday_index_members")
-system("Rscript \"N:/Depts/Share/UK Alpha Team/Analytics/Rscripts/intraday_bank_pairs.R\"")
-
-
-#
-append2log("workflow: create_tsne_grid")
-system("Rscript \"N:/Depts/Share/UK Alpha Team/Analytics/Rscripts/create_tsne_grid.R\"")
-
-#
-append2log("workflow: perform_sheet_scrape_to_db")
-system("Rscript \"N:/Depts/Share/UK Alpha Team/Analytics/Rscripts/perform_sheet_scrape_to_db.R\"")
-
-
-#
-append2log("workflow: create_pair_icons")
-system("Rscript \"N:/Depts/Share/UK Alpha Team/Analytics/Rscripts/create_pair_icons.R\"")
-
-
-#
-# data has been produced, now make .PDF reports
-#
-
-#system("Rscript \"N:/Depts/Share/UK Alpha Team/Analytics/Rscripts/create_risk_reports.R\"")
-
-
 append2log("workflow: create_risk_reports")
-
 setwd("N:/Depts/Share/UK Alpha Team/Analytics/risk_reports")
 create_report("scrape_status")
 create_report("market_data_status")
