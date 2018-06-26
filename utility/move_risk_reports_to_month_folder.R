e4b3765641a50942a893086489642205c010488e
nassuphis <nassuphis@users.noreply.github.com>
2018-06-26 12:42:16 +0100

    Update move_risk_reports_to_month_folder.R

--- utility/move_risk_reports_to_month_folder.R
+++ utility/move_risk_reports_to_month_folder.R
@@ -1,3 +1,41 @@
+#
+#
+#
+#
 
+
+
+move_risk_reports_to_month_folder<-function(config){
+month_folder<-fread("
+    month,  folder     
+    01,     january
+    02,     february
+    03,     march
+    04,     april
+    05,     may
+    06,     june
+    07,     july
+    08,     august
+    09,     september
+    10,     october
+    11,     november
+    12,     december
+",
+colClasses=c(month="character",folder="character")
+)[,.SD,keyby=month]
+  
+  for(i in month_folder$month){
+    file_pattern<-paste0("^[a-z_]{1,30}_20[0-9]{2}-",i,"-*")
+    the_files<-list.files(config$risk_report_directory,file_pattern)
+    the_directory<-paste0(config$risk_report_directory,"/",month_folder[i,folder])
+    if(length(the_files)>0)for(f in the_files){
+      file.copy(
+        paste0(config$risk_report_directory,"/",f),
+        paste0(the_directory,"/",f)
+      )
+      file.remove(paste0(config$risk_report_directory,"/",f))
+    }
+  }  
+}
 
 
