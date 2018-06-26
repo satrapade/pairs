55ffeb9d6c2fb6e1f723f9aa56fdbb55851d8888
nassuphis <nassuphis@users.noreply.github.com>
2018-06-26 08:43:58 +0100

    Update and rename intraday_bank_pairs.R to intraday_index_members.R

--- workflow/intraday_index_members.R
+++ workflow/intraday_index_members.R
@@ -13,21 +13,14 @@ library(Rblpapi)
 conn <- new(BlpApiConnection)
 rcon<-Rblpapi::blpConnect()
 
-append2log<-function(log_text,append=TRUE)
-{
-  cat(
-    paste0(stri_trim(gsub("##|-","",capture.output(timestamp())))," : ",log_text,"\n"),
-    file="N:/Depts/Share/UK Alpha Team/Analytics/Rscripts/workflow.log",
-    append=append
-  )
-}
+source("https://raw.githubusercontent.com/satrapade/pairs/master/utility/append2log.R")
 
-append2log("intraday_bank_pairs: source utility_functions, sheet_bbg_functions")
+append2log("intraday_index_members: source utility_functions, sheet_bbg_functions")
 source("https://raw.githubusercontent.com/satrapade/utility/master/utility_functions.R")
 source("https://raw.githubusercontent.com/satrapade/utility/master/sheet_bbg_functions.R")
 
 
-append2log("intraday_bank_pairs: fetch index memebers")
+append2log("intraday_index_members: fetch index memebers")
 indices<-c("SX8P Index","SX5E Index","SX7E Index","SX7P Index","SXIP Index","SXIE Index")
 
 get_index_members<-function(ndx){
@@ -42,13 +35,13 @@ get_index_members<-function(ndx){
 
 index_membership<-do.call(rbind,mapply(get_index_members,indices,SIMPLIFY = FALSE))
 
-append2log("intraday_bank_pairs: save market_data/index_membership.csv ")
+append2log("intraday_index_members: save market_data/index_membership.csv ")
 fwrite(index_membership,"N:/Depts/Share/UK Alpha Team/Analytics/market_data/index_membership.csv")
 
 
 all_tickers<-sort(unique(index_membership$ticker))
 
-append2log("intraday_bank_pairs: fetch intraday for index members")
+append2log("intraday_index_members: fetch intraday for index members")
 bars<-mapply(function(ticker){
   res<-Rblpapi::getBars(
     ticker,
@@ -59,7 +52,7 @@ bars<-mapply(function(ticker){
   data.table(ticker=rep(ticker,nrow(res)),res)
 },all_tickers,SIMPLIFY=FALSE)
 
-append2log("intraday_bank_pairs: save market_data/intraday_index_members.csv")
+append2log("intraday_index_members: save market_data/intraday_index_members.csv")
 intraday<-do.call(rbind,bars[mapply(nrow,bars)>0])
 fwrite(intraday,"N:/Depts/Share/UK Alpha Team/Analytics/market_data/intraday_index_members.csv")
 
@@ -91,7 +84,7 @@ make_bar_matrix<-function(x,minute_interval=10,extrapolate=TRUE){
   x4
 }
 
-append2log("intraday_bank_pairs: compute intraday_open")
+append2log("intraday_index_members: compute intraday_open")
 intraday_open<-local({
   res<-rename_colnames(dMcast(intraday,times~ticker,value.var="open"),"^ticker","")
   make_bar_matrix(res)
@@ -103,26 +96,26 @@ intraday_close<-local({
   make_bar_matrix(res)
 })
 
-append2log("intraday_bank_pairs: compute intraday_high")
+append2log("intraday_index_members: compute intraday_high")
 intraday_high<-local({
   res<-rename_colnames(dMcast(intraday,times~ticker,value.var="high"),"^ticker","")
   make_bar_matrix(res)
 })
 
-append2log("intraday_bank_pairs: compute intraday_low")
+append2log("intraday_index_members: compute intraday_low")
 intraday_low<-local({
   res<-rename_colnames(dMcast(intraday,times~ticker,value.var="low"),"^ticker","")
   make_bar_matrix(res)
 })
 
-append2log("intraday_bank_pairs: compute intraday_perf")
+append2log("intraday_index_members: compute intraday_perf")
 intraday_perf<-structure(
   (tail(intraday_close,-1)-head(intraday_close,-1))/head(intraday_close,-1),
   dimnames=list(tail(rownames(intraday_close),-1),colnames(intraday_close)),
   datetime=tail(attributes(intraday_close)$datetime,-1)
 )
 
-append2log("intraday_bank_pairs: compute bar_intervals")
+append2log("intraday_index_members: compute bar_intervals")
 bar_moves<-rowSums(abs(intraday_perf)>0)
 bar_moves<-c(0,roll_mean(rowSums(abs(intraday_perf)>0),2))
 bar_open<-c(head(bar_moves>ncol(intraday_perf)/4,-1),FALSE)
@@ -144,15 +137,15 @@ bar_intervals<-data.table(
   day=ifelse(open>close,open,0)
 ))]
 
-append2log("intraday_bank_pairs: save market_data/intraday_perf_index_members.csv")
+append2log("intraday_index_members: save market_data/intraday_perf_index_members.csv")
 fwrite(data.table(intraday_perf),"N:/Depts/Share/UK Alpha Team/Analytics/market_data/intraday_perf_index_members.csv")
 
-append2log("intraday_bank_pairs: save market_data/intraday_open_index_members.csv")
+append2log("intraday_index_members: save market_data/intraday_open_index_members.csv")
 fwrite(data.table(intraday_open),"N:/Depts/Share/UK Alpha Team/Analytics/market_data/intraday_open_index_members.csv")
 
-append2log("intraday_bank_pairs: save market_data/intraday_close_index_members.csv")
+append2log("intraday_index_members: save market_data/intraday_close_index_members.csv")
 fwrite(data.table(intraday_close),"N:/Depts/Share/UK Alpha Team/Analytics/market_data/intraday_close_index_members.csv")
 
-append2log("intraday_bank_pairs: save market_data/bar_intervals_index_members.csv")
+append2log("intraday_index_members: save market_data/bar_intervals_index_members.csv")
 fwrite(data.table(bar_intervals),"N:/Depts/Share/UK Alpha Team/Analytics/market_data/bar_intervals_index_members.csv")
 
