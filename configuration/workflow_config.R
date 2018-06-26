541a183c3097dcdf689181d4b49870f9b1b4ed9c
nassuphis <nassuphis@users.noreply.github.com>
2018-06-26 09:40:21 +0100

    Update workflow_config.R

--- configuration/workflow_config.R
+++ configuration/workflow_config.R
@@ -5,7 +5,14 @@
 
 # data preparation steps
 workflow = list(
-  "create_database_temp_tables"=list(),
+  "create_database_temp_tables"=list(
+    database_product_id="7",
+    database_source_id="1",
+    database_start_date="'2015-05-28'",
+    database_end_date="'2018-05-01'",
+    database_root_bucket_name="'EqyBucket'",
+    database_results_directory="N:/Depts/Share/UK Alpha Team/Analytics/db_cache/"
+  ),
   "create_cix_uploads"=list(),
   "create_portfolio_upload"=list(),
   "create_market_data"=list(),
