a7ff5f7a09a3171e5027011ccca5f126b56ee0f2
nassuphis <nassuphis@users.noreply.github.com>
2018-06-26 12:50:09 +0100

    Update move_risk_reports_to_month_folder.R

--- utility/move_risk_reports_to_month_folder.R
+++ utility/move_risk_reports_to_month_folder.R
@@ -3,29 +3,27 @@
 #
 #
 
-
-
 move_risk_reports_to_month_folder<-function(config){
-month_folder<-fread("
-    month,  folder     
-    01,     january
-    02,     february
-    03,     march
-    04,     april
-    05,     may
-    06,     june
-    07,     july
-    08,     august
-    09,     september
-    10,     october
-    11,     november
-    12,     december
-",
+  month_folder<-fread("
+                      month,  folder     
+                      01,     january
+                      02,     february
+                      03,     march
+                      04,     april
+                      05,     may
+                      06,     june
+                      07,     july
+                      08,     august
+                      09,     september
+                      10,     october
+                      11,     november
+                      12,     december
+                      ",
 colClasses=c(month="character",folder="character")
 )[,.SD,keyby=month]
   
   for(i in month_folder$month){
-    file_pattern<-paste0("^[a-z_]{1,30}_20[0-9]{2}-",i,"-*")
+    file_pattern<-paste0("^[A-Za-z_]{1,30}_20[0-9]{2}-",i,"-*")
     the_files<-list.files(config$risk_report_directory,file_pattern)
     the_directory<-paste0(config$risk_report_directory,"/",month_folder[i,folder])
     if(length(the_files)>0)for(f in the_files){
@@ -37,5 +35,3 @@ colClasses=c(month="character",folder="character")
     }
   }  
 }
-
-
