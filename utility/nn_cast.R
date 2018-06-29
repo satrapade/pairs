f5ff71cbd319f12d2883c01e7eb67dff82433b0b
nassuphis <nassuphis@users.noreply.github.com>
2018-06-29 12:24:10 +0100

    Create nn_cast.R

---
+++ utility/nn_cast.R
@@ -0,0 +1,38 @@
+require(data.table)
+
+NNcast<-function(
+  data,
+  i_name="date",
+  j_name="id",
+  v_name="value",
+  fun=sum,
+  scrub_fun=function(x)scrub(x,default=0),
+  scrub=function(x, default = 0){
+    if(length(x) == 0) return(default)
+    x[which(!is.finite(x))] <- default
+    return(x)
+  },
+  default_value=NA
+){
+  i_expr<-parse(text=as.character(i_name))
+  j_expr<-parse(text=as.character(j_name))
+  v_expr<-parse(text=as.character(v_name))
+  i<-as.character(eval(i_expr,envir=data))
+  j<-as.character(eval(j_expr,envir=data))
+  x<-eval(v_expr,envir=data)
+  df<-data.table(i=i,j=j,x=x)[,.(x=fun(x)),keyby="i,j"]
+  is<-sort(unique(df$i))
+  js<-sort(unique(df$j))
+  res<-matrix(
+    default_value,
+    nrow=length(is),
+    ncol=length(js),
+    dimnames = list(is,js)
+  )
+  i<-match(df$i,rownames(res))
+  j<-match(df$j,colnames(res))
+  res[cbind(i,j)[!is.na(df$x),]]<-df$x[!is.na(df$x)]
+  scrub_fun(res)
+}
+
+
