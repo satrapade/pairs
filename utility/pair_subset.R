4db01eb1cec468a662f3ff1b1b327af774b76b73
nassuphis <nassuphis@users.noreply.github.com>
2018-07-01 12:56:12 +0100

    Update pair_subset.R

--- utility/pair_subset.R
+++ utility/pair_subset.R
@@ -5,7 +5,7 @@ require(magrittr)
 pair_subset<-function(
   manager="*",
   cutoff="2018-06-01",
-  filter=grepl(manager,bucket),
+  filter=grepl(dataset$manager,dataset$bucket),
   dataset=duke
 )(
 dataset[filter] %>%
