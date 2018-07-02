b6f335f59e0c5f6ec77979ac4a5e9a602e83c466
nassuphis <nassuphis@users.noreply.github.com>
2018-07-02 15:14:55 +0100

    Update create_report.R

--- utility/create_report.R
+++ utility/create_report.R
@@ -1,6 +1,7 @@
 #
-create_report<-function(
+create_report_new<-function(
   report_name,
+  output_suffix="",
   push_to_directory=NULL,
   config=get("config",parent.frame()),
   envir=new.env()
@@ -19,10 +20,10 @@ create_report<-function(
     setwd(wd)
     return(paste0(report_name,": error: report does not exist"))
   }
-  append2log(paste0(report_name,": creating report"))
+  append2log(paste0(report_name,output_suffix,": creating report"))
   
   # remove figures 
-  append2log(paste0(report_name,": deleting old figures, old .tex and pdf files"))
+  append2log(paste0(report_name,output_suffix,": deleting old figures, old .tex and pdf files"))
   temp_files<-list.files(
     path=paste0(config$risk_report_directory,"/figure"),
     pattern="pdf$",
@@ -30,29 +31,36 @@ create_report<-function(
     full.names = TRUE
   )
   file.remove(temp_files)
-  if(file.exists(paste0(report_name,".tex")))file.remove(paste0(report_name,".tex"))
-  if(file.exists(paste0(report_name,".pdf")))file.remove(paste0(report_name,".pdf"))
+  if(file.exists(paste0(report_name,output_suffix,".tex")))file.remove(paste0(report_name,".tex"))
+  if(file.exists(paste0(report_name,output_suffix,".pdf")))file.remove(paste0(report_name,".pdf"))
   
   # knit .Rnw file
   append2log(paste0(report_name,": knitting Rnw:"))
   res<-try(knit(
     input=paste0(config$risk_report_directory,"/",report_name,".Rnw"),
-    output=paste0(config$risk_report_directory,"/",report_name,".tex"),
+    output=paste0(config$risk_report_directory,"/",report_name,output_suffix,".tex"),
     envir=envir
   ),silent = TRUE)
+  
   if(class(res) %in% "try-error"){
     append2log(paste0(report_name,": !!!>ERROR<!!! :",gsub("\n","",as.character(res))))
     setwd(wd)
-    return(paste0(report_name,": error: tex file not generated"))
+    return(paste0(report_name,output_suffix,": error: tex file not generated"))
   }
-  if(!file.exists(paste0(report_name,".tex"))){
-    append2log(paste0(report_name,": !!!>ERROR<!!! :.tex file not generated"))
+  
+  if(!file.exists(paste0(report_name,output_suffix,".tex"))){
+    append2log(paste0(report_name,output_suffix,": !!!>ERROR<!!! :.tex file not generated"))
     setwd(wd)
-    return(paste0(report_name,": error: tex file not generated"))
+    return(paste0(report_name,output_suffix,": error: tex file not generated"))
   }
   # pdf latex, append date-time
-  append2log(paste0(report_name,": pdflatex on"))  
-  outfn<-paste0(report_name,"_",gsub("\\s","x",gsub(":","",as.character(Sys.timeDate()))))
+  append2log(paste0(report_name,output_suffix,": pdflatex on"))  
+  outfn<-paste0(
+    report_name,
+    output_suffix,
+    "_",
+    Sys.timeDate() %>% as.character %>% {gsub("\\s","x",gsub(":","",.))}
+  )
   pdf_latex_cmd<-paste0(
     "pdflatex  -jobname=\"",
     outfn,
@@ -60,12 +68,13 @@ create_report<-function(
     config$risk_report_directory,
     "/",
     report_name,
+    output_suffix,
     ".tex\""
   )
   system(pdf_latex_cmd)
   
   # delete temporary latex files
-  append2log(paste0(report_name,": remove temporary latex files"))  
+  append2log(paste0(report_name,output_suffix,": remove temporary latex files"))  
   temp_files<-list.files(
     path="N:/Depts/Share/UK Alpha Team/Analytics/risk_reports",
     pattern="(dpth$)|(log$)|(md5$)|(aux$)|(gz$)|(toc$)|(out$)",
@@ -75,25 +84,42 @@ create_report<-function(
   file.remove(temp_files)
   
   # make on-the run copy
-  append2log(paste0(report_name,": copying to latest"))  
-  file.copy(paste0(outfn,".pdf"),paste0(report_name,".pdf"))
+  append2log(paste0(report_name,output_suffix,": copying to latest"))  
+  file.copy(paste0(outfn,".pdf"),paste0(report_name,output_suffix,".pdf"))
   if(!file.exists(paste0(report_name,".pdf"))){
-    append2log(paste0(report_name,": !!!>ERROR<!!! :.pdf file not generated:"))
+    append2log(paste0(report_name,output_suffix,": !!!>ERROR<!!! :.pdf file not generated:"))
     setwd(wd)
-    return(paste0(report_name," error: pdf file not generated"))
+    return(paste0(report_name,output_suffix," error: pdf file not generated"))
   }
   if(!is.null(push_to_directory)){
     file.copy(paste0(outfn,".pdf"),paste0(push_to_directory,outfn,".pdf"))
   }
-  if(file.exists(paste0(report_name,".tex")))file.remove(paste0(report_name,".tex"))
-  if(file.exists(paste0(report_name,".toc")))file.remove(paste0(report_name,".toc"))
-  if(file.exists(paste0(report_name,".out")))file.remove(paste0(report_name,".out"))
-  if(file.exists(paste0(report_name,"-concordance.tex")))file.remove(paste0(report_name,"-concordance.tex"))
-  if(file.exists(paste0(outfn,".tex")))file.remove(paste0(outfn,".tex"))
-  if(file.exists(paste0(outfn,".toc")))file.remove(paste0(outfn,".toc"))
-  if(file.exists(paste0(outfn,".out")))file.remove(paste0(outfn,".out"))
-  if(file.exists(paste0(outfn,"-concordance.tex")))file.remove(paste0(outfn,"-concordance.tex"))
+  if(file.exists(paste0(report_name,output_suffix,".tex"))){
+    file.remove(paste0(report_name,output_suffix,".tex"))
+  }
+  if(file.exists(paste0(report_name,output_suffix,".toc"))){
+    file.remove(paste0(report_name,output_suffix,".toc"))
+  }
+  if(file.exists(paste0(report_name,output_suffix,".out"))){
+    file.remove(paste0(report_name,output_suffix,".out"))
+  }
+  if(file.exists(paste0(report_name,output_suffix,"-concordance.tex"))){
+    file.remove(paste0(report_name,output_suffix,"-concordance.tex"))
+  }
+  if(file.exists(paste0(outfn,".tex"))){
+    file.remove(paste0(outfn,".tex"))
+  }
+  if(file.exists(paste0(outfn,".toc"))){
+    file.remove(paste0(outfn,".toc"))
+  }
+  if(file.exists(paste0(outfn,".out"))){
+    file.remove(paste0(outfn,".out"))
+  }
+  if(file.exists(paste0(outfn,"-concordance.tex"))){
+    file.remove(paste0(outfn,"-concordance.tex"))
+  }
   
   setwd(wd)
-  return(paste0(report_name,": success"))
+  return(paste0(report_name,output_suffix,": success"))
 }
+
