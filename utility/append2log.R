17924fadbab78ccaf95927ad2df1b5a15965aa3c
nassuphis <nassuphis@users.noreply.github.com>
2018-07-12 10:32:48 +0100

    Update append2log.R

--- utility/append2log.R
+++ utility/append2log.R
@@ -2,6 +2,7 @@ require(stringi)
 
 append2log<-function(log_text,append=TRUE)
 {
+  if(Sys.info()["sysname"]!="Windows")return()
   cat(
     paste0(stri_trim(gsub("##|-","",capture.output(timestamp())))," : ",log_text,"\n"),
     file="N:/Depts/Share/UK Alpha Team/Analytics/Rscripts/workflow.log",
